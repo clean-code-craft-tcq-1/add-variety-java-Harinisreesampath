24afec08878c8411edd758c164db22b16f54570c
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-04-03 11:26:51 +0530

    Add files via upload

--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,6 +1,11 @@
 package TypewiseAlert;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import TypewiseAlert.BatteryCharacter;
+
+import java.lang.reflect.InvocationTargetException;
 
 import org.junit.Test;
 
@@ -9,7 +14,17 @@ public class TypewiseAlertTest
     @Test
     public void infersBreachAsPerLimits()
     {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
-        TypewiseAlert.BreachType.TOO_LOW);
+      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==  TypewiseAlert.BreachType.TOO_LOW);      
+    }
+    
+    @Test
+    public void classifiesTemperatureBreachType() {
+        assertTrue(TypewiseAlert.classifyTemperatureBreach(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING, 50) == TypewiseAlert.BreachType.TOO_HIGH);
+    }
+    
+    @Test
+    public void checkingAndAlertingTheUser() {
+        BatteryCharacter batteryCharacter  = new BatteryCharacter(TypewiseAlert.CoolingType.HI_ACTIVE_COOLING,"Brand"); 
+        TypewiseAlert.checkAndAlert(TypewiseAlert.AlertTarget.TO_EMAIL,batteryCharacter, 60);
     }
 }
